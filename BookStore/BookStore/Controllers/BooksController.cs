a590486df778dba7f08618991306def05b65108e
ranu1490 <ranusinghrajput75@gmail.com>
2022-04-09 17:21:10 +0530

    BookStore

---
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+       /* public IActionResult Index()
+        {
+            return View();
+        }*/
+        public string Index()
+        {
+            return "This is the book index.";
+        }
+
+        public string Details()
+        {
+            return "This is the details of a book.";
+        }
+    }
+}
